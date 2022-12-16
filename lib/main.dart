4d08a9718ec02f8bf5002fcd508f88293f9692db
MrSR2000 <94947959+MrSR2000@users.noreply.github.com>
2022-12-16 13:50:16 +0545

    SecretKey

--- lib/main.dart
+++ lib/main.dart
@@ -51,8 +51,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int? _primeNo;
+  int? _privateKey1;
+  int? _publicKey1;
+  int? _primitiveRoot;
+  int? _privateKey2;
+  int? _publicKey2;
+  int? _secretKey1;
+  int? _secretKey2;
 
-  @override
+  @immutable
   int getRandomInt() {
     var now = new DateTime.now();
     Random rnd1 = new Random();
@@ -111,6 +118,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
     }
     print('the primitive set is $primitives');
+
+    Random random = Random();
+    int randomPrimitiveRoot = random.nextInt(primitives.length);
+    final List<int> primitiveRootList = primitives.toList();
+    // print(primitiveRootList);
+
+    setState(() {
+      _primitiveRoot = primitiveRootList[randomPrimitiveRoot];
+    });
+
+    print(_primitiveRoot);
+  }
+
+  getPrivateKey(int user) {
+    Random random = Random();
+    setState(() {
+      if (user == 1) {
+        _privateKey1 = random.nextInt(_primeNo!);
+      } else if (user == 2) {
+        _privateKey2 = random.nextInt(_primeNo!);
+      }
+    });
+    print('the Private Key is $_privateKey1');
+    print('the Private Key is $_privateKey2');
+  }
+
+  getPublicKey(int user) {
+    setState(() {
+      if (user == 1) {
+        _publicKey1 = (pow(_primitiveRoot!, _privateKey1!)).toInt() % _primeNo!;
+      } else if (user == 2) {
+        _publicKey2 = (pow(_primitiveRoot!, _privateKey2!)).toInt() % _primeNo!;
+      }
+    });
+    print('the Public Key is $_publicKey1');
+    print('the Public Key is $_publicKey2');
+  }
+
+  getSecretKey(int user) {
+    setState(() {
+      if (user == 1) {
+        _secretKey1 = (pow(_publicKey2!, _privateKey1!)).toInt() % _primeNo!;
+      } else if (user == 2) {
+        _secretKey2 = (pow(_publicKey1!, _privateKey2!)).toInt() % _primeNo!;
+      }
+    });
+    print('Secret key of user1 = $_secretKey1');
+    print('Secret key of user2 = $_secretKey2');
   }
 
   @override
@@ -127,10 +182,40 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           ElevatedButton(
             onPressed: () {
-              print('the random prime no is $_primeNo');
+              getPrivateKey(1);
             },
-            child: Text('get primitive set'),
-          )
+            child: Text('get PrivateKey user 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              getPublicKey(1);
+            },
+            child: Text('get PublicKey user 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              getPrivateKey(2);
+            },
+            child: Text('get PrivateKey user 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              getPublicKey(2);
+            },
+            child: Text('get PublicKey user 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              getSecretKey(1);
+            },
+            child: Text('Get Secret key of user1'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              getSecretKey(2);
+            },
+            child: Text('Get Secret key of user2'),
+          ),
         ],
       ),
     );
